248f97cc1850748137db5fb41e78877e1f2957b7
Andre Ciornavei <andre.ciornavei@gmail.com>
2020-06-17 21:56:39 -0300

    Exported RDS endpoint to SSM

--- terraform/infra/system/auroradb.tf
+++ terraform/infra/system/auroradb.tf
@@ -16,3 +16,9 @@ resource "aws_rds_cluster_instance" "rds_cluster_instance" {
   instance_class      = "db.t2.small"
   publicly_accessible = true
 }
+
+resource "aws_ssm_parameter" "ssm_rds_cluster_instance_endpoint" {
+  name    = "${var.environment}-ssm-rds-cluster-instance-endpoint"
+  type    = "String"
+  value   = aws_rds_cluster_instance.rds_cluster_instance.endpoint
+}
